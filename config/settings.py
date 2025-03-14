3607c273fa070ac54f35e33b6d83553e3d0c4e0f
pcm422 <pcm0422@gmail.com>
2025-03-14 15:50:40 +0900

    📝 update settings

--- config/settings.py
+++ config/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = DATABASE
+DATABASES = DATABASES
 
 
 # Password validation
