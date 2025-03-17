43d2a5fa77b35c1377d05582038bf4fbd138c6f2
Ibiella <rubyhae@gmail.com>
2025-03-17 16:07:22 +0900

    :sparkles: updated Preferences models

--- Preferences/models.py
+++ Preferences/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from Accounts.models import User
+from Idols.models import Group
 
-# Create your models here.
+
+class UserGroupSubscribe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    notification = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user} subscribed to {self.group} (Notification: {self.notification})"
