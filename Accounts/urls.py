90139717b0d5818edf789a49c097c9f8dd318be8
pcm422 <pcm0422@gmail.com>
2025-03-20 12:46:02 +0900

    ✨ create find_email api

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import LoginView, LogoutView, RegisterView, SocialLoginView, VerifyEmailView
+from .views import (
+    FindEmailView,
+    LoginView,
+    LogoutView,
+    RegisterView,
+    SocialLoginView,
+    VerifyEmailView,
+)
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
@@ -8,4 +15,5 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", RegisterView.as_view(), name="register"),
     path("verify-email/", VerifyEmailView.as_view(), name="verify_email"),
+    path("find-email/", FindEmailView.as_view(), name="find_email"),
 ]
