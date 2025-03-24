fa6a12f336c19dc6915ba12c6cda3d3484a8e465
Ibiella <rubyhae@gmail.com>
2025-03-24 11:05:35 +0900

    :memo: genericview 코드 리팩토링, 시작과 끝 날짜 validate 추가

--- Schedules/serializer.py
+++ Schedules/serializer.py
@@ -7,9 +7,15 @@ from .models import Schedule
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
-    participating_members = IdolSerializer(many=True, read_only=True)
+    participating_members = serializers.PrimaryKeyRelatedField(many=True, queryset=Idol.objects.all())
+    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all())
 
     class Meta:
         model = Schedule
         fields = ["group", "title", "description", "location", "start_time", "end_time", "participating_members"]
+
+    def validate(self,data):
+        if data ['end_time'] <= data['start_time']:
+            raise serializers.ValidationError("end_time은 start_time 이후여야 합니다.")
+        return data
 
--- Schedules/views.py
+++ Schedules/views.py
@@ -1,84 +1,47 @@
-from django.db.models import prefetch_related_objects
-from django.shortcuts import get_object_or_404
-from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Schedule
 from .permissions import IsAdminOrReadOnly
 from .serializer import ScheduleSerializer
 
 
-class ScheduleListView(APIView):
+class ScheduleListView(ListCreateAPIView):
     """
     일정 목록 조회 및 등록
     """
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
-    def get(self, request):
-        schedules = Schedule.objects.all()
-        serializer = ScheduleSerializer(schedules, many=True)
-        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        # 일정 등록
-        serializer = ScheduleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
-        return Response(
-            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
-        )
-
-
-class ScheduleDetailView(APIView):
+class ScheduleDetailView(RetrieveUpdateDestroyAPIView):
     """
     특정 일정 상세 조회, 수정 및 삭제
     """
-
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
     permission_classes = [IsAdminOrReadOnly]
 
-    def get(self, request, pk):
-        # 일정 상세 조회
-        schedule = get_object_or_404(Schedule, pk=pk)
-        serializer = ScheduleSerializer(schedule)
-        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
-
-    def patch(self, request, pk):
-        # 일정 부분 수정
-        schedule = get_object_or_404(Schedule, pk=pk)
-        serializer = ScheduleSerializer(schedule, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"data": serializer.data}, status=status.HTTP_200_OK)
-        return Response(
-            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        # 일정 삭제
-        schedule = get_object_or_404(Schedule, pk=pk)
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
         deleted_schedule_data = {
-            "schedule.id": schedule.id,
-            "group_id": schedule.group_id,
-            "title": schedule.title,
+            "schedule.id": instance.id,
+            "group_id": instance.group_id,
+            "title": instance.title,
         }
+        self.perform_destroy(instance)
 
-        schedule.delete()
         return Response(
             {"data": deleted_schedule_data},
-            status=status.HTTP_200_OK,
         )
 
-
-class GroupScheduleListView(APIView):
+class GroupScheduleListView(ListAPIView):
     """
     특정 그룹의 일정 필터링
     """
+    serializer_class = ScheduleSerializer
 
-    def get(self, request, group_id):
-        # 특정 그룹의 일정 필터링
-        schedules = (Schedule.objects.filter(group_id=group_id).prefetch_related("participating_members"))
-
-        serializer = ScheduleSerializer(schedules, many=True)
-        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+    def get_queryset(self):
+        group_id = self.kwargs["group_id"]
+        return Schedule.objects.filter(group_id=group_id).prefetch_related("participating_members")
 
