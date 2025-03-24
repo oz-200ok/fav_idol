96b2459055d742683bfbd3fa39b62c0c1ffd12be
Ibiella <rubyhae@gmail.com>
2025-03-24 10:12:50 +0900

    :memo: updated view in schedule as serializer has been changed

--- Schedules/views.py
+++ Schedules/views.py
@@ -1,3 +1,4 @@
+from django.db.models import prefetch_related_objects
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
@@ -10,28 +11,51 @@ from .serializer import ScheduleSerializer
 
 class ScheduleListView(APIView):
     """
-    일정 목록 조회
+    일정 목록 조회 및 등록
     """
 
     def get(self, request):
         schedules = Schedule.objects.all()
         serializer = ScheduleSerializer(schedules, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        # 일정 등록
+        serializer = ScheduleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class ScheduleDetailView(APIView):
     """
-    특정 일정 상세 조회 및 삭제
+    특정 일정 상세 조회, 수정 및 삭제
     """
 
     permission_classes = [IsAdminOrReadOnly]
 
     def get(self, request, pk):
+        # 일정 상세 조회
         schedule = get_object_or_404(Schedule, pk=pk)
         serializer = ScheduleSerializer(schedule)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+    def patch(self, request, pk):
+        # 일정 부분 수정
+        schedule = get_object_or_404(Schedule, pk=pk)
+        serializer = ScheduleSerializer(schedule, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     def delete(self, request, pk):
+        # 일정 삭제
         schedule = get_object_or_404(Schedule, pk=pk)
         deleted_schedule_data = {
             "schedule.id": schedule.id,
@@ -41,48 +65,9 @@ class ScheduleDetailView(APIView):
 
         schedule.delete()
         return Response(
-            {
-                "message": "일정이 삭제되었습니다.",
-                "deleted_schedule_data": deleted_schedule_data,
-            },
+            {"data": deleted_schedule_data},
             status=status.HTTP_200_OK,
         )
-
-
-class ScheduleCreateView(APIView):
-    """
-    새 일정 생성
-    """
-
-    permission_classes = [IsAdminOrReadOnly]
-
-    def post(self, request):
-        serializer = ScheduleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ScheduleUpdateView(APIView):
-    """
-    일정 업데이트
-    """
-
-    permission_classes = [IsAdminOrReadOnly]
-
-    def post(self, request, pk):
-        schedule = get_object_or_404(Schedule, pk=pk)
-
-        # 기존 객체 업데이트를 위해 instance 전달
-        serializer = ScheduleSerializer(instance=schedule, data=request.data)
-        if serializer.is_valid():
-            # 데이터 저장
-            serializer.save()
-
-            # 업데이트 된 데이터를 반환
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GroupScheduleListView(APIView):
@@ -92,6 +77,8 @@ class GroupScheduleListView(APIView):
 
     def get(self, request, group_id):
         # 특정 그룹의 일정 필터링
-        schedules = Schedule.objects.filter(group_id=group_id)
+        schedules = (Schedule.objects.filter(group_id=group_id).prefetch_related("participating_members"))
+
         serializer = ScheduleSerializer(schedules, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
