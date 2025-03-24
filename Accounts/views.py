274e03996a7dd124744c174632a02ac86ea3b8e5
pcm422 <pcm0422@gmail.com>
2025-03-24 13:32:40 +0900

    🐛 refactor userprofileupdateview
    불필요하게 들어있던 코드 지우고 리턴을 커스텀 리스폰스로 수정

--- Accounts/views.py
+++ Accounts/views.py
@@ -166,25 +166,26 @@ class UserProfileView(generics.RetrieveAPIView):
 class UserProfileUpdateView(generics.UpdateAPIView):
     serializer_class = UserProfileUpdateSerializer
     permission_classes = [IsAuthenticated]
-
+    
     def get_object(self):
         return self.request.user
-
+    
+    def perform_update(self, serializer):
+        return serializer.save()
+        
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop("partial", False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-
-        return Response(
+        
+        return custom_response(
             {
-                "data": {
-                    "username": instance.username,
-                    "phone": instance.phone,
-                }
+                "username": instance.username,
+                "phone": instance.phone,
             },
-            status=status.HTTP_201_CREATED,
+            status_code=status.HTTP_201_CREATED
         )
 
 
