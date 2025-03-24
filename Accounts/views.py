80a75bd7bd9dbb8ced0086d3743188371100417f
pcm422 <pcm0422@gmail.com>
2025-03-24 11:22:32 +0900

    🐛 refactor loginview
    불필요하게 들어있던 코드 지우고 리턴을 커스텀 리스폰스로 수정

--- Accounts/views.py
+++ Accounts/views.py
@@ -23,24 +23,18 @@ def custom_response(data=None, status_code=status.HTTP_200_OK):
 class LoginView(generics.CreateAPIView):
     serializer_class = LoginSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data["user"]
-        access_token = serializer.validated_data["access_token"]
-        refresh_token = serializer.validated_data["refresh_token"]
-        expires_in = serializer.validated_data["expires_in"]
+    def perform_create(self, serializer):
+        return serializer.validated_data
 
-        return Response(
+    def create(self, request, *args, **kwargs):
+        data = self.perform_create(self.get_serializer(data=request.data))
+        return custom_response(
             {
-                "data": {
-                    "access_token": access_token,
-                    "refresh_token": refresh_token,
-                    "expires_in": expires_in,
-                    "user": {"id": user.id, "email": user.email},
-                }
-            },
-            status=status.HTTP_200_OK,
+                "access_token": data["access_token"],
+                "refresh_token": data["refresh_token"],
+                "expires_in": data["expires_in"],
+                "user": {"id": data["user"].id, "email": data["user"].email},
+            }
         )
 
 
