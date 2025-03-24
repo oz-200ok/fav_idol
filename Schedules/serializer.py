d27dc0c4f80a572a70d361fea5fd21821353b821
Ibiella <rubyhae@gmail.com>
2025-03-24 10:12:14 +0900

    :memo: updated serializer in schedule for efficiency

--- Schedules/serializer.py
+++ Schedules/serializer.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 
 from Idols.models import Group, Idol
+from Idols.serializers import IdolSerializer
 
 from .models import Schedule
 
 
 class ScheduleSerializer(serializers.ModelSerializer):
-    group = serializers.PrimaryKeyRelatedField(queryset=Group.objects.all())
-    participating_members = serializers.SlugRelatedField(
-        many=True, slug_field="name", queryset=Idol.objects.all()
-    )
+    participating_members = IdolSerializer(many=True, read_only=True)
 
     class Meta:
         model = Schedule
-        fields = "__all__"
+        fields = ["group", "title", "description", "location", "start_time", "end_time", "participating_members"]
+
