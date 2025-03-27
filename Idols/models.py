1d3b78018e35fc6429838a75172b0dd210ef78bb
Ibiella <rubyhae@gmail.com>
2025-03-27 13:10:07 +0900

    :bug:  modified null option for image

--- Idols/models.py
+++ Idols/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Agency(models.Model):
     name = models.CharField(max_length=20)  # null 불가, 공백 불가
     contact = models.CharField(max_length=50, null=True)
-    image = models.URLField(max_length=500)
+    image = models.URLField(max_length=500, null=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +23,7 @@ class Group(models.Model):
         ],
     )
     sns = models.URLField(blank=True, null=True)  # SNS 링크
-    image = models.URLField(max_length=500)  # 그룹 이미지
+    image = models.URLField(max_length=500, null=True)  # 그룹 이미지
 
     def __str__(self):
         return f"{self.name} ({self.agency.name})"
@@ -32,7 +32,7 @@ class Group(models.Model):
 class Idol(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
     name = models.CharField(max_length=10)
-    image = models.URLField(max_length=500)
+    image = models.URLField(max_length=500, null=True)
 
     def __str__(self):
         return f"{self.name} ({self.group.name})"
