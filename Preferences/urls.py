a44bd1080e6797a8e184bd2cd25bb9d5a0b86e7d
pcm422 <pcm0422@gmail.com>
2025-03-31 13:05:38 +0900

    ✨ create NotificationSettingsView's urls

--- Preferences/urls.py
+++ Preferences/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
-from .views import SubscribeViewSet, UserScheduleDetailView, UserSubscribedSchedulesView
+from .views import NotificationSettingsView, SubscribeViewSet, UserScheduleDetailView, UserSubscribedSchedulesView
 
 router = DefaultRouter()
 router.register(r"subscriptions", SubscribeViewSet, basename="subscribe")
@@ -11,4 +11,5 @@ urlpatterns = [
     *router.urls,
     path('schedules/', UserSubscribedSchedulesView.as_view(), name='user-subscribed-schedules'),    
     path('schedules/<int:schedule_id>/', UserScheduleDetailView.as_view(), name='user-schedule-detail'),
+    path('notification-settings/', NotificationSettingsView.as_view(), name='notification-settings'),
 ]
