6e0c65cf8a0c3cba456a9c36c05baa732ff87ee4
Ibiella <rubyhae@gmail.com>
2025-04-01 13:08:50 +0900

    :bug: added blank true to image field

--- Idols/models.py
+++ Idols/models.py
@@ -7,7 +7,7 @@ from Accounts.models import User
 class Agency(models.Model):
     name = models.CharField(max_length=20)  # null 불가, 공백 불가
     contact = models.CharField(max_length=50, null=True)
-    image = models.URLField(max_length=500, null=True)
+    image = models.URLField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.name
